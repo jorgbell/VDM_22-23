477dadf6595c64ae172d3b4f6381ca1dafd678d8
Tomás López Antón <tomalope@ucm.es>
2022-11-29 23:33:03 +0100

    Lectura de tablero pseudocodigo

--- Practicas/P2/Logic/src/main/java/com/nonogram/logic/TableroCargado.java
+++ Practicas/P2/Logic/src/main/java/com/nonogram/logic/TableroCargado.java
@@ -18,7 +18,25 @@ public class TableroCargado extends Tablero{
 
         for(int i = 0; i < _rowNumber; i++) completaFila(i);
         for(int i = 0; i < _columnNumber; i++) completaColumna(i);
+        //cargaEstadoTablero();
     }
+
+//    private void cargaEstadoTablero() {
+//        for(int i = 0; i < _rowNumber; i++) {
+//            for(int j = 0; j < _columnNumber; j++){
+//                _tablero[i][j].setState((State)_board.Solucion[i][j]);
+//            }
+//        }
+//    }
+//
+//    public void setEstadoTablero()
+//    {
+//        for(int i = 0; i < _rowNumber; i++) {
+//            for(int j = 0; j < _columnNumber; j++){
+//                _board.Solucion _tablero[i][j].setState((State)_board.Solucion[i][j]);
+//            }
+//        }
+//    }
 
     JSONManager.BoardData _board;
 }
